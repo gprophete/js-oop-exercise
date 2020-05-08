03e66f36516faf2cfab759cb7616de7159c62896
gprophete <garlyprophete30@gmail.com>
2020-05-07 21:54:57 -0400

    part 5 added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -7,6 +7,7 @@ class Tamagotchi {
         this.health = 10
         this.name = name
         this.creatureType = creatureType
+
     }
     cry() {
         this.foodInTummy--;
@@ -23,6 +24,20 @@ class Tamagotchi {
         console.log(this.name +" "+ "has current restedness of:" +" "+ this.restedness)
     }
 
+    start(){
+        const hungerTimer = setInterval(cry(), 6000);
+        console.log (hungerTimer)
+        const yawnTimer = setInterval(yawn(), 10000);
+        console.log(yawnTimer)
+        const sickTimer = setInterval(puke(), 20000);
+        console.log(sickTimer)
+    }
+
+    stop(){
+        const clear = clearInterval(hungerTimer, yawnTimer, sickTimer)
+        console.log(clear)
+    }
+
 }
 
 const tam1 = new Tamagotchi('Hidru', 'Faeries')
@@ -34,5 +49,7 @@ tam1.puke();
 tam2.puke();
 tam1.yawn();
 tam2.yawn();
+
+
 
 console.log(tam1.yawn())
