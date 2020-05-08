eb309b98e8b82da2989dfe9cc7176da34a4340ea
gprophete <garlyprophete30@gmail.com>
2020-05-07 21:25:38 -0400

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -13,11 +13,26 @@ class Tamagotchi {
         console.log(this.foodInTummy)
         console.log('Waaahhh!!!!')
     }
+    puke() {
+        this.foodInTummy--;
+        console.log(this.foodInTummy +" "+ "hullabaloo")
+    }
+
+    yawn(){
+        this.restedness--;
+        console.log(this.name +" "+ "has current restedness of:" +" "+ this.restedness)
+    }
 
 }
 
-const tam1 = new Tamagotchi(Hidru, Faeries)
-const tam2 = new Tamagotchi(Stomera, Goblins)
+const tam1 = new Tamagotchi('Hidru', 'Faeries')
+const tam2 = new Tamagotchi('Stomera', 'Goblins')
 
 tam1.cry();
-tam2.cry()
+tam2.cry();
+tam1.puke();
+tam2.puke();
+tam1.yawn();
+tam2.yawn();
+
+console.log(tam1.yawn())
