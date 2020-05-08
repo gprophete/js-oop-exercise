5d562d02301a25192a85a162d45f8028a40cdb81
gprophete <garlyprophete30@gmail.com>
2020-05-07 21:04:48 -0400

    part 2: added arguments to Tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,10 +1,12 @@
 // console.log('tamagotchi file is loaded');
 
 class Tamagotchi {
-    constructor(foodInTummy, restedness, health) {
+    constructor(foodInTummy, restedness, health, name, creatureType) {
         this.foodInTummy = 10
         this.restedness = 10
         this.health = 10
+        this.name = name
+        this.creatureType = creatureType
     }
     cry() {
         this.foodInTummy--;
@@ -14,8 +16,8 @@ class Tamagotchi {
 
 }
 
-const tam1 = new Tamagotchi()
-const tam2 = new Tamagotchi()
+const tam1 = new Tamagotchi(Hidru, Faeries)
+const tam2 = new Tamagotchi(Stomera, Goblins)
 
 tam1.cry();
 tam2.cry()
